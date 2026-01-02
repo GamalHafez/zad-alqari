c473244f8e778de612bc4c4662eecc5042535fe2
GamalHafez <gamal.k.hafez@gmail.com>
2026-01-02 20:14:37 +0200

    style: adjust books grid columns for responsive layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <SearchForm />
-      <section className="mt-3 mb-8 grid grid-cols-1 justify-items-center gap-x-8 gap-y-10 p-5 md:grid-cols-3 lg:grid-cols-3">
+      <section className="mt-3 mb-8 grid grid-cols-1 justify-items-center gap-x-8 gap-y-10 p-5 md:grid-cols-2 lg:grid-cols-3">
         {books.map((book) => (
           <BlogCard key={book.id} book={book} />
         ))}
