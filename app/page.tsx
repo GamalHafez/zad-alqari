0d4e1b10f97b09b820b8e1dd1d4a4940f6b80d33
GamalHafez <gamal.k.hafez@gmail.com>
2026-01-09 20:18:31 +0200

    feat: add client-side search for books by title and author

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
+"use client";
 import BlogCard from "@/components/blog-card";
 import SearchForm from "@/components/search-form";
-import { books } from "@/data/books";
+import { Book, books } from "@/data/books";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [filteredBooks, setFilteredBooks] = useState(books);
+  const updateBooks = (books: Book[]) => setFilteredBooks(books);
+
   return (
     <>
-      <SearchForm />
+      <SearchForm updateBooks={updateBooks} />
       <section className="mt-3 mb-8 grid grid-cols-1 justify-items-center gap-x-8 gap-y-10 p-5 md:grid-cols-2 lg:grid-cols-3">
-        {books.map((book) => (
+        {filteredBooks.map((book) => (
           <Link key={book.id} href={`/books/${book.slug}`}>
             <BlogCard book={book} />
           </Link>
--- components/search-form/SearchForm.tsx
+++ components/search-form/SearchForm.tsx
@@ -1,10 +1,37 @@
 "use client";
 
+import { Book, books } from "@/data/books";
 import SearchButton from "./search-button";
 
-const SearchForm = () => {
+const findBooks = (books: Book[], query: string) => {
+  const lowerCaseQuery = query.toLowerCase();
+  return books.filter((book) => {
+    return (
+      book.title.toLowerCase().includes(lowerCaseQuery) ||
+      book.author.toLowerCase().includes(lowerCaseQuery)
+    );
+  });
+};
+
+const SearchForm = ({
+  updateBooks,
+}: {
+  updateBooks: (books: Book[]) => void;
+}) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const searched = formData.get("search-form") as string;
+
+    const filteredBooks = findBooks(books, searched.trim());
+    filteredBooks ? updateBooks(filteredBooks) : updateBooks([]);
+  };
+
   return (
-    <form className="mt-12 flex max-w-3xs place-self-center overflow-hidden rounded-3xl border border-gray-300 md:w-1/2 lg:w-1/2">
+    <form
+      onSubmit={handleSubmit}
+      className="mt-12 flex max-w-3xs place-self-center overflow-hidden rounded-3xl border border-gray-300 md:w-1/2 lg:w-1/2"
+    >
       <input
         type="search"
         name="search-form"
