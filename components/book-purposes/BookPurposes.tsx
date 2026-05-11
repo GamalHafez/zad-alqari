81985cde5872085a7167e7313a25f7454d39e936
GamalHafez <gamal.k.hafez@gmail.com>
2026-05-11 22:56:25 +0300

    refactor: remove background styling from large screen breakpoint in button class

--- components/book-purposes/BookPurposes.tsx
+++ components/book-purposes/BookPurposes.tsx
@@ -8,7 +8,7 @@ const BookPurposes = ({ purposes }: { purposes: string[] }) => {
         {purposes.map((purpose, i) => (
           <li
             key={i}
-            className="flex place-items-baseline gap-2 rounded-md px-4 py-2 text-sm text-gray-800 md:gap-3 lg:gap-3 lg:bg-gray-50"
+            className="flex place-items-baseline gap-2 rounded-md px-4 py-2 text-sm text-gray-800 md:gap-3 lg:gap-3"
           >
             <span className="h-1.5 w-1.5 rounded-full bg-blue-900"></span>
             <span>{purpose}</span>
